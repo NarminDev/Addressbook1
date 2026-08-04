c42316c7b90f4b096c01c39d533a2b2b14ebf11e
NarminDev <narmintp-bpa101@code.edu.az>
2026-08-04 23:55:45 +0400

    Register login

---
+++ Addressbook1/Controllers/AccountController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Addressbook1.Controllers
+{
+    public class AccountController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Addressbook1/ViewModels/Account/LoginVM.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Addressbook1.ViewModels.Account
+{
+    public record LoginVM
+    {
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
