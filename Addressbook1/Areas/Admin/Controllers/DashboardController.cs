cfb49d81107fc80c7a7b15520bade1ba933669ad
NarminDev <narmintp-bpa101@code.edu.az>
2026-08-04 15:32:43 +0400

    4th commit

---
+++ Addressbook1/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Addressbook1.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
