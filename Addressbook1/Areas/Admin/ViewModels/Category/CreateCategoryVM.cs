5edab9dd9602afc11acc53306631712e55229ca3
NarminDev <narmintp-bpa101@code.edu.az>
2026-08-04 19:14:51 +0400

    The beginning of category CRUD

---
+++ Addressbook1/Areas/Admin/ViewModels/Category/CreateCategoryVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Addressbook1.Areas.Admin.ViewModels.Category
+{
+    public record CreateCategoryVM
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(30, ErrorMessage = "Name can not exceed 30 characters")]
+        [MinLength(3, ErrorMessage = "Name must be at least 3 characters")]
+        public string Name { get; set; }
+    }
+}
