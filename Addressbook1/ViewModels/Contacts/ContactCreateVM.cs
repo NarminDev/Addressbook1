dc9e1e173667063254137814278c09db9923f09f
NarminDev <narmintp-bpa101@code.edu.az>
2026-08-07 03:08:28 +0400

    ContactCreateVM added

---
+++ Addressbook1/ViewModels/Contacts/ContactCreateVM.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Addressbook1.ViewModels.Contacts
+{
+    public record ContactCreateVM
+    {
+        [Required(ErrorMessage = "Ad daxil edilməlidir")]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Soyad daxil edilməlidir")]
+        [StringLength(50)]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Telefon nömrəsi daxil edilməlidir")]
+        public string Phone { get; set; }
+
+        // Əgər kateqoriya mütləqdirsə, bunu da əlavə edirik
+        public int CategoryId { get; set; }
+    }
+}
