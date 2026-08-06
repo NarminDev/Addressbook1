22104f1f1077d342556dbfd6e41be5d1dddefea7
NarminDev <narmintp-bpa101@code.edu.az>
2026-08-07 03:19:51 +0400

    ContactController-Index,Create

---
+++ Addressbook1/Controllers/ContactController.cs
@@ -0,0 +1,36 @@
+﻿using Addressbook1.DAL;
+using Addressbook1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Addressbook1.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly AppDbContext _db;
+        private readonly IWebHostEnvironment _env;
+
+        public ContactController(AppDbContext db, IWebHostEnvironment env)
+        {
+            _db = db;
+            _env = env;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Contact> contacts = await _db.Contacts
+                .Include(c => c.Category)
+                .ToListAsync();
+            return View(contacts);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.Categories = await _db.Categories.ToListAsync();
+            return View();
+        }
+
+
+
+
+    }
+}
