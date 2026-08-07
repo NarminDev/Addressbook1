9a32d388ae4a26069c3e3de5b67715f28257828a
NarminDev <narmintp-bpa101@code.edu.az>
2026-08-07 18:48:16 +0400

    DashboardVM entered

---
+++ Addressbook1/Areas/Admin/ViewModels/Dashboard/DashboardVM.cs
@@ -0,0 +1,10 @@
+﻿namespace Addressbook1.Areas.Admin.ViewModels.Dashboard
+{
+    public record DashboardVM
+    {
+        public int TotalUsers { get; set; }
+        public int TotalContacts { get; set; }
+        public string TopCategoryName { get; set; }
+        public int TopCategoryCount { get; set; }
+    }
+}
